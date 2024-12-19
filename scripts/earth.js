bf4aa9952bc62935e01d2b61c327e93ac3e0ba0a
sgaleraalq <sgaleraalq@gmail.com>
2024-12-19 14:26:15 +0100

    Small updates

--- scripts/earth.js
+++ scripts/earth.js
@@ -1,19 +1,22 @@
-import * as THREE from "https://cdn.skypack.dev/three@0.150.0";
+import * as THREE from "file:///C:/Users/D817238/Documents/other/three/build/three.js";
 
 console.log('THREE REVISION: %c${THREE.REVISION}', 'color: red');
 window.THREE = THREE;
 
-// const earthGlobe = document.getElementById("earth-globe");
-const w = window.innerWidth
-const h = window.innerHeight;
+const earthGlobe = document.getElementById("earth-globe");
+// const w = window.innerWidth
+// const h = window.innerHeight;
+const w = earthGlobe.clientWidth;
+const h = earthGlobe.clientHeight;
+
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, w / h, 0.1, 1000);
 camera.position.z = 5;
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(w, h);
-document.body.appendChild(renderer.domElement);
-// earthGlobe.appendChild(renderer.domElement);
+// document.body.appendChild(renderer.domElement);
+earthGlobe.appendChild(renderer.domElement);
 
 const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshStandardMaterial({
--- scripts/trial.js
+++
@@ -1,1 +0,0 @@
-import { OrbitControls } from 'https://cdn.skypack.dev/npm/three@0.150.0/addons/controls/OrbitControls.js';
