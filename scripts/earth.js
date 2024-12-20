11cc61a7fa10336f58bc4d881f09e093ab506701
sgaleraalq <sgaleraalq@gmail.com>
2024-12-20 10:36:57 +0100

    Earth implementation created

--- scripts/earth.js
+++ scripts/earth.js
@@ -5,27 +5,30 @@ console.log('THREE REVISION: %c${THREE.REVISION}', 'color: red');
 window.THREE = THREE;
 
 const earthGlobe = document.getElementById("earth-globe");
-// const w = window.innerWidth
-// const h = window.innerHeight;
-const w = earthGlobe.clientWidth;
-const h = earthGlobe.clientHeight;
+const w = window.innerWidth
+const h = window.innerHeight;
+// const w = earthGlobe.clientWidth;
+// const h = earthGlobe.clientHeight;
 
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, w / h, 0.1, 1000);
 camera.position.z = 5;
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(w, h);
-// document.body.appendChild(renderer.domElement);
-earthGlobe.appendChild(renderer.domElement);
+document.body.appendChild(renderer.domElement);
+// earthGlobe.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry();
+new OrbitControls(camera, renderer.domElement);
+
+const loader = new THREE.TextureLoader();
+const geometry = new THREE.IcosahedronGeometry(1, 12);
 const material = new THREE.MeshStandardMaterial({
-    color: 0x00ff00
+    map: loader.load('../textures/earthmap1k.jpg')
 });
 
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+const earthMesh = new THREE.Mesh(geometry, material);
+scene.add(earthMesh);
 
 const hemiLight = new THREE.HemisphereLight(0xffffbb, 0x080820, 1);
 scene.add(hemiLight);
@@ -33,8 +36,8 @@ scene.add(hemiLight);
 function animate() {
     requestAnimationFrame(animate);
 
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+    earthMesh.rotation.x += 0.001;
+    earthMesh.rotation.y += 0.001;
     renderer.render(scene, camera);
 }
 
