9019bd37cff3a4c694bd8f4dbeb201eb98d95a87
sgaleraalq <sgaleraalq@gmail.com>
2024-12-20 11:45:23 +0100

    Stars added to scene

--- scripts/earth.js
+++ scripts/earth.js
@@ -1,16 +1,16 @@
 import * as THREE from "./three/build/three.module.js";
 import { OrbitControls } from './three/examples/jsm/controls/OrbitControls.js';
+import getStarfield from './getStarField.js';
 
-console.log('THREE REVISION: %c${THREE.REVISION}', 'color: red');
 window.THREE = THREE;
-
 const earthGlobe = document.getElementById("earth-globe");
 const w = window.innerWidth
 const h = window.innerHeight;
 // const w = earthGlobe.clientWidth;
 // const h = earthGlobe.clientHeight;
 
-
+const earthGroup = new THREE.Group();
+earthGroup.rotation.z = -23.4 * Math.PI / 180;
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, w / h, 0.1, 1000);
 camera.position.z = 5;
@@ -30,14 +30,16 @@ const material = new THREE.MeshStandardMaterial({
 const earthMesh = new THREE.Mesh(geometry, material);
 scene.add(earthMesh);
 
+const stars = getStarfield();
+scene.add(stars);
+
 const hemiLight = new THREE.HemisphereLight(0xffffbb, 0x080820, 1);
 scene.add(hemiLight);
 
 function animate() {
     requestAnimationFrame(animate);
 
-    earthMesh.rotation.x += 0.001;
-    earthMesh.rotation.y += 0.001;
+    earthMesh.rotation.y += 0.002;
     renderer.render(scene, camera);
 }
 
