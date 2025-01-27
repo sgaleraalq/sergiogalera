a0090e5129215d34e4d40870e28eef47e1c4cd70
sgaleraalq <sgaleraalq@gmail.com>
2025-01-27 11:03:43 +0100

    Trying to inject containers into main

--- scripts/projects.js
+++ scripts/projects.js
@@ -1,7 +1,34 @@
+let projectsData
 
 fetch("../static/projects.json")
     .then((response) => response.json())
     .then((data) => {
-        console.log(data);
+        projectsData = data;
     })
     .catch((error) => console.log("Error: ", error));
+
+
+
+    function displayProjects() {
+        console.log("Displaying projects..."); // Verifica que la función se llame
+        const projectsContainer = document.getElementById("projects-list-container");
+        
+        if (!projectsContainer) {
+            console.error("projects-list-container not found!"); // Verifica que el contenedor exista
+            return;
+        }
+    
+        fetch("../components/html/projects_container.html")
+            .then((response) => response.text())
+            .then((data) => {
+                console.log("Data loaded:", data); // Verifica que los datos se carguen
+                const tempDiv = document.createElement("div");
+                tempDiv.innerHTML = data;
+                projectsContainer.appendChild(tempDiv.firstElementChild);
+            })
+            .catch((error) => {
+                console.error("Error loading projects:", error); // Captura errores
+            });
+    }
+    
+    document.addEventListener('DOMContentLoaded', displayProjects);
