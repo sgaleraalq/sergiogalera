8f3f7d79c86ce9718bdeb495c2b44560dc692d27
sgaleraalq <sgaleraalq@gmail.com>
2025-02-04 14:07:35 +0100

    Website linking to projects

--- scripts/projects.js
+++ scripts/projects.js
@@ -35,52 +35,61 @@ document.addEventListener("DOMContentLoaded", async function () {
     // Cargar la plantilla projects_container.html
     const template = await loadComponent();
 
-    // for (let index = 0; index < data.length; index++) {
-    //     const project = data[index];
-
-    //     // Solo procesar el primer proyecto
-    //     if (index === 0) {
-    //         const tempContainer = document.createElement("div");
-    //         tempContainer.innerHTML = template;
-
-    //         const title = tempContainer.querySelector("#title");
-    //         title.textContent = project.title;  // Cambiar el texto del título
-
-    //         // If project is reversed
-    //         if (project.reverse) {
-    //             tempContainer.querySelector(".main-container").classList.add("reverse")
-    //         }
-
-    //         // Añadir el contenido al contenedor
-    //         projectsContainer.innerHTML += tempContainer.innerHTML;
-
-    //         break;  // Detener la iteración después del primer elemento
-    //     }
-
-    //     if (index % 2 == 1) {
-    //         tempContainer.querySelector(".main-container").classList.add("reverse")
-    //     }
-    // }
-
     data.forEach((project, index) => {
         const tempContainer = document.createElement("div");
         tempContainer.innerHTML = template;
 
 
-        const title = tempContainer.querySelector("#title")
-        title.textContent = project.title
-
+        // TEXT CONTAINERS
+        tempContainer.querySelector("#title").textContent = project.title;
+        tempContainer.querySelector("#technology").textContent = project.technology;
+        tempContainer.querySelector("#description").textContent = project.description;
+        tempContainer.querySelector("#description").innerHTML = project.description.replace(/\n/g, '<br>');
         
+        // PROJECTS CONTAINER
+        project.skills.forEach(skill => {
+            const skillElement = document.createElement("p");
+            skillElement.classList.add("skill");
+            skillElement.textContent = skill;
+            tempContainer.querySelector(".skills-container").appendChild(skillElement);
+        })
+
+        // LINKS CONTAINER
+        const linksContainer = tempContainer.querySelector(".links-container");
+        displayLinks(linksContainer, project.github_url, "ic_github.svg");
+        displayLinks(linksContainer, project.playstore_url, "ic_playstore.svg");
+        
+        // IF PROJECT IS ODD, REVERSE THE CONTAINERS
         if (index % 2 == 1) {
-            tempContainer.querySelector(".main-container").classList.add("reverse")
-            tempContainer.querySelector(".info-container").classList.add("reverse")
-            tempContainer.querySelector(".technology-container").classList.add("reverse")
-            tempContainer.querySelector(".skills-container").classList.add("reverse")
-            tempContainer.querySelector(".project-image").classList.add("reverse")
-            tempContainer.querySelector(".title").classList.add("reverse")
-            tempContainer.querySelector(".description").classList.add("reverse")
+            tempContainer.querySelector(".main-container").classList.add("reverse");
+            tempContainer.querySelector(".info-container").classList.add("reverse");
+            tempContainer.querySelector(".technology-container").classList.add("reverse");
+            tempContainer.querySelector(".skills-container").classList.add("reverse");
+            tempContainer.querySelector(".project-image").classList.add("reverse");
+            tempContainer.querySelector(".title").classList.add("reverse");
+            tempContainer.querySelector(".description").classList.add("reverse");
         }
         projectsContainer.innerHTML += tempContainer.innerHTML;
 
     })
 });
+
+
+function displayLinks(container, link, icon) {
+    if (link && link.trim() !== "") {
+        // Create link and open in a new tab
+        const anchor = document.createElement("a");
+        anchor.href = link;
+        anchor.target = "_blank";
+        anchor.classList.add("link"); 
+
+        // Adds icon to website
+        const img = document.createElement("img");
+        img.src = `/static/icons/${icon}`; 
+        img.alt = icon.split(".")[0];
+        img.classList.add("svg");
+
+        anchor.appendChild(img);
+        container.appendChild(anchor);
+    }
+}
