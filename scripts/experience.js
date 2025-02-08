0c56c2fcf76fa0e78f6241e9ec96a531088f10bd
sgaleraalq <sgaleraalq@gmail.com>
2025-02-08 15:01:29 +0100

    Updating experience

--- scripts/experience.js
+++ scripts/experience.js
@@ -9,6 +9,8 @@ document.addEventListener('DOMContentLoaded', async function(){
 
 
 function loadTimeline(data) {
+    const timeline = document.querySelector("#timeline");
+
     let experienceYears = [];
     let intershipYears = [];
 
@@ -21,7 +23,27 @@ function loadTimeline(data) {
         }
     });
 
-    
+    const firstYear = Math.min(...experienceYears, ...intershipYears);
+    const now = new Date().getFullYear();
+
+    loadTimelineItem(experienceYears, "timeline-experience-item", firstYear, now);
+    loadTimelineItem(intershipYears, "timeline-experience-item", firstYear, now);
+}
+
+function loadTimelineItem(yearList, element, firstYear, now) {
+    yearList.forEach(year => {
+        console.log(year)
+        const yearElement = document.createElement("div");
+        yearElement.classList.add(element);
+
+        // Position percentage of the year
+        const percentage = (year - firstYear) / (now - firstYear) * 100;
+        yearElement.style.left = `${percentage}%`;
+        yearElement.style.marginLeft = "-15px";
+        yearElement.setAttribute("data-year", year);
+
+        timeline.appendChild(yearElement);
+    })
 }
 
 function loadExperience(data){
