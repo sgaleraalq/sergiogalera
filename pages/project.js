45cfe2bdf04f8455986b1e6a3d87d15d2a3b41d9
sgaleraalq <sgaleraalq@gmail.com>
2025-02-14 12:11:17 +0100

    Some restructure of project

--- pages/project.js
+++ pages/project.js
@@ -1,10 +1,15 @@
 
+const projectsPath = "/static/projects/";
+const projectId = new URLSearchParams(window.location.search).get("projectId");
+
 document.addEventListener('DOMContentLoaded', async function() {
     await loadProject();
 });
 
 
 async function loadProject() {
-    const data = await loadData("/static/projects/gaztelu_bira/description.json");
-    console.log(data);
+    const data = await loadData(projectsPath + projectId + "/description.json");
+
+    const title     = data.title;
+    const logo      = data.logo;
 }
